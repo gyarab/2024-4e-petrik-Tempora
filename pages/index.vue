e54529cdff63517ce7ad1eb7b988be6c21bde3e0
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-11-25 18:44:22 +0100

    more default pages

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Welcome to site of interactive time </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+    h2{
+        font-size: 36px;
+        margin-bottom: 20px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
