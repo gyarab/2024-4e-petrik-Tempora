79d9bc35661595fa146aef00ac5ce05c1f2f6148
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-11-26 14:45:09 +0100

    added dynamic id route for custom maps

---
+++ pages/maps/[id].vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Map page of {{ id }}</h2>
+        <p>Welcome to site of interactive time </p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style  scoped>
+    h2{
+        font-size: 36px;
+        margin-bottom: 20px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/maps/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Maps hub page</h2>
+        <p>Maps created: </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+    h2{
+        font-size: 36px;
+        margin-bottom: 20px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
