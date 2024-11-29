c171e69542cf3abbf2e6593328539bee6dc22478
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-11-29 12:06:48 +0100

    addded font support and roboto font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@nuxt/icon'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+  ],
   tailwindcss: {
     // Add Tailwind dark mode configuration here
     config: {
@@ -19,5 +24,16 @@ export default defineNuxtConfig({
   },
   supabase: {
     redirect: false
-    },
+  },
+  fonts: {
+    families: [
+      {
+        name: 'Roboto',
+        provider: 'google',
+        weights: [400, 700], // Regular and Bold weights
+        styles: ['normal', 'italic'], // Normal and Italic styles
+      },
+    ],
+  },
+  
 })
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Roboto as default sans-serif font
+      },
+    },
+  },
+  plugins: [],
+};
