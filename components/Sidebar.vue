9e9974bf1b171ae813ac982514325a9ede30bd1d
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-12-24 15:12:25 +0100

    removed bun for consistency and speed

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed white-black top-16 left-0 h-screen w-24  
-                flex flex-col shadow-lg bg-sky-900 dark:bg-sky-300"> 
+                flex flex-col shadow-lg bg-sky-950"> 
                 <Icon class=" sidebar-icon" name="material-symbols:settings" />
                 <Icon class=" sidebar-icon" name="material-symbols:settings" />
                 <Icon class=" sidebar-icon" name="material-symbols:settings" />
