f805a99488c55fe98de94ea5a2ea7f38359dac9f
Jiriik <Jiri.petrik@student.gyarab.cz>
2025-02-21 16:13:20 +0100

    about page update

--- pages/about.vue
+++ pages/about.vue
@@ -2,26 +2,38 @@
     <div class="container_box" >
         <div class="content_box h-full w-full mx-10">
             <div class="text-center">
-            <h2 >About page</h2>
+            <h2 > O projektu Tempora</h2>
         </div>
-            <p>Welcome to site of interactive time </p>
-            <p>Webová aplikace představující historickou interaktivní časovou osu české a zahraniční literatury v kontextu významných světových událostí. 
-                Uživatelé budou moci snadno procházet literární směry, významné autory a jejich klíčová díla, s možností rozkliknutí pro podrobnější 
-                informace o jednotlivých obdobích a autorech. Aplikace umožní jednoduché přidávání nových položek na časovou osu na základě zadaných 
-                údajů a vytvářeních nových časových os např. s jiným tématem.</p>
+         
+
+         <!-- O projektu -->
+        <div>
+            <p>Tempora je univerzální nástroj pro tvorbu interaktivních časových os, který umožňuje vizualizovat historické události, osobnosti a kontexty v širších souvislostech. Hlavním cílem projektu je usnadnit organizaci a porozumění historickým a tematickým souvislostem prostřednictvím intuitivního rozhraní.</p>  
+            <br>
+            <p> Klíčové vlastnosti: </p>
+                
+            <p> <span class="font-bold">Interaktivní prvky</span>  -  Události, osobnosti a historické směry lze snadno prozkoumávat a zobrazovat v detailním kontextu. </p>
+            <p> <span class="font-bold">Flexibilita</span> - Možnost vytvářet a upravovat vlastní časové osy s přizpůsobitelnými prvky. </p>
+            <p> <span class="font-bold">Moderní technologie</span> - Tempora je postavena na Nuxt.js pro frontend a Supabase jako backendovou databázi.</p>
+        </div>  
+
+
+        <!-- Odkazy -->
+        <div>
+            <a
+            href="https://github.com/gyarab/2024-4e-petrik-Tempora"
+            target="_blank"
+            class="icon-style"
+            >
+            <div>
+                <span>
+                <Icon class="size-16 text-gray-950 " name="uil:github"  /> <br>
+                </span>
+                <span class="text-gray-950"> GitHub </span> 
+            </div>
+            </a>
+        </div>
             
-                <a
-                href="https://github.com/gyarab/2024-4e-petrik-Tempora"
-                target="_blank"
-                class="icon-style"
-                >
-                <div>
-                    <span>
-                    <Icon class="size-16 text-gray-950 " name="uil:github"  /> <br>
-                    </span>
-                    <span class="text-gray-950"> GitHub </span> 
-                </div>
-                </a>
                
         </div>
     </div>
