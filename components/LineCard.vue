d0e09ea5cfd67033fe08a71334ea5fa5d24733c9
Jiriik <Jiri.petrik@student.gyarab.cz>
2025-02-21 17:43:58 +0100

    line card style fix

--- components/LineCard.vue
+++ components/LineCard.vue
@@ -1,36 +1,44 @@
 <template>
-    <div class="line-card border border-gray-300 p-4 rounded-lg bg-gray-50 shadow-sm dark:bg-gray-800 dark:border-gray-950" >
+  <div class="line-card border-2 border-black p-4 rounded-lg bg-white dark:bg-zinc-800 shadow-md flex flex-col justify-between h-full">
+    <div>
       <h3 class="font-bold text-lg">{{ line.name }}
         <UIcon v-if="line.is_private" class="size-4" name="heroicons:lock-closed-16-solid"></UIcon>
         <UIcon v-if="line.featured" class="size-4" name="heroicons:star-16-solid"></UIcon>
       </h3>
-      <p class="text-sm text-gray-600 dark:text-gray-300">Roky: {{ displayStartYear  }} - {{ displayEndYear }}</p>
-      <p class="text-sm text-gray-600 dark:text-gray-300">Autor: {{ line.user_profiles?.nickname || 'Unknown' }}</p>
-        
-      <div class="text-base container mt-2">
-        <!-- Only if user UUID = the Line author UUID  -->
-        <button @click="handleInfoToggle(line.line_id)" class="mr-2"> 
+      <p class="text-sm text-gray-600 dark:text-gray-300">Roky: {{ displayStartYear }} - {{ displayEndYear }}</p>
+      <p class="text-sm text-gray-600 dark:text-gray-300">
+        Autor: 
+        <UTooltip v-if="nickname.length > 16" :text="nickname">
+          <span>{{ truncatedNickname }}</span>
+        </UTooltip>
+        <span v-else>{{ nickname }}</span>
+      </p>
+    </div>
+    
+    <div class="mt-auto flex flex-col">
+      <div class="flex items-center mt-2">
+        <button @click="handleInfoToggle(line.line_id)" class="mr-4"> 
           <UTooltip text="Info" :popper="{ placement: 'right' }">
             <Icon class="size-8" name="uil:info-circle"/>
           </UTooltip>
         </button> 
 
-        <button v-if="user" @click="handleSettingsToggle(line.line_id)" > 
+        <button v-if="user" @click="handleSettingsToggle(line.line_id)"> 
           <UTooltip text="Nastavení" :popper="{ placement: 'right' }">
             <Icon class="size-8" name="uil:setting"/>
           </UTooltip>
         </button> 
       </div>
 
-      
-
       <UButton 
-        class="btn text-white px-4 py-2 rounded mt-2" 
+        class="skyButton px-4 py-2 mt-2" 
         :to="`/lines/${line.line_id}`" 
         label="Přejít na osu">
       </UButton>
     </div>
-  </template>
+  </div>
+</template>
+
   
   <script setup>
   const user = useSupabaseUser()
@@ -60,5 +68,16 @@ const displayStartYear = computed(() => {
 const displayEndYear = computed(() => {
   return props.line.end < 0 ? `${Math.abs(props.line.end)} BC` : props.line.end;
 });
+
+const nickname = computed(() => {
+  return props.line.user_profiles?.nickname || 'Unknown'
+})
+
+const truncatedNickname = computed(() => {
+  if (nickname.value.length > 16) {
+    return `${nickname.value.substring(0, 13)}...`
+  }
+  return nickname.value
+})
 
 </script>
